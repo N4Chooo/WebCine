3a9ce14b758749b6de928e6091af1ad6dc4957f3
Nacho Fayos <nafama@alumnatflorida.es>
2026-01-20 12:45:51 +0100

    commit2

--- php/index.php
+++ php/index.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
+<head> 
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Listado de Películas</title>
